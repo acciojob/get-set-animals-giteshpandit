49213b524bd264141f9566037aeb14cb2568ac2b
giteshpandit <panditgitesh38@gmail.com>
2024-10-14 11:54:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,60 @@
-//complete this code
-class Animal {}
+// Animal Class
+class Animal {
+  constructor(species) {
+    this._species = species;
+  }
 
-class Dog extends Animal {}
+  // Getter for species
+  get species() {
+    return this._species;
+  }
 
-class Cat extends Animal {}
+  // Method to make a generic sound
+  makeSound() {
+    console.log(`The ${this._species} makes a sound`);
+  }
+}
 
-// Do not change the code below this line
-window.Animal = Animal;
-window.Dog = Dog;
-window.Cat = Cat;
+// Cat Class extending Animal
+class Cat extends Animal {
+  constructor(species) {
+    super(species);
+  }
+
+  // Method to purr
+  purr() {
+    console.log("purr");
+  }
+
+  // Optionally, override makeSound if you want a specific cat sound
+  // makeSound() {
+  //   console.log(`${this.species} says meow`);
+  // }
+}
+
+// Dog Class extending Animal
+class Dog extends Animal {
+  constructor(species) {
+    super(species);
+  }
+
+  // Method to bark
+  bark() {
+    console.log("woof");
+  }
+
+  // Optionally, override makeSound if you want a specific dog sound
+  // makeSound() {
+  //   console.log(`${this.species} says bark`);
+  // }
+}
+
+// Example Usage
+const myCat = new Cat("Siamese");
+myCat.makeSound(); // Output: The Siamese makes a sound
+myCat.purr();      // Output: purr
+
+const myDog = new Dog("Golden Retriever");
+myDog.makeSound(); // Output: The Golden Retriever makes a sound
+myDog.bark();      // Output: woof
+
