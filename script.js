7bef599d6f782787f4cbc8381afc5385b81c1709
giteshpandit <panditgitesh38@gmail.com>
2024-10-16 11:49:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,47 +1,45 @@
-// Animal class
+// Base class Animal
 class Animal {
   constructor(species) {
     this._species = species;
   }
-
+  
   // Getter for species
   get species() {
     return this._species;
   }
-
-  // Method to make a generic sound
+  
+  // Method to make sound
   makeSound() {
-    console.log(`The ${this._species} makes a sound`);
+    console.log(`The ${this.species} makes a sound`);
   }
 }
 
-// Dog class extending Animal
-class Dog extends Animal {
-  constructor(species) {
-    super(species);
-  }
-
-  // Method to bark
-  bark() {
-    console.log("woof");
-  }
-}
-
-// Cat class extending Animal
+// Subclass Cat extends Animal
 class Cat extends Animal {
-  constructor(species) {
-    super(species);
-  }
-
-  // Method to purr
+  // Method specific to Cat
   purr() {
     console.log("purr");
   }
 }
 
-// Do not change the code below this line
-window.Animal = Animal;
-window.Dog = Dog;
-window.Cat = Cat;
+// Subclass Dog extends Animal
+class Dog extends Animal {
+  // Method specific to Dog
+  bark() {
+    console.log("woof");
+  }
+}
+
+// Example usage
+const myCat = new Cat("Siamese");
+myCat.makeSound(); // Output: The Siamese makes a sound
+myCat.purr(); // Output: purr
+
+const myDog = new Dog("Golden Retriever");
+myDog.makeSound(); // Output: The Golden Retriever makes a sound
+myDog.bark(); // Output: woof
+
+
 
 
